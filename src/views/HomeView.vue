f2f9e4edb2108df18d555a3291e40c4412b7c700
Daniel Kweon <danielkweon19@gmail.com>
2023-11-10 00:46:49 -0500

    fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,71 @@
 <script setup>
+import { ref } from 'vue';
+import searchSVG from '../assets/icons/search.svg';
+
+const url = ref('');
+const errorMessage = ref('');
+const isError = ref('');
+const usesHttps = ref(false);
+const dataProcessed = ref(false);
+
+function processURL() {
+  resetValues();
+  if(url.value != ""){
+    try {
+      const fccUrl = new URL(url.value);
+      usesHttps.value = checkHttps(fccUrl); 
+
+      dataProcessed.value = true;
+    } catch (err) {
+      console.log(err)
+      isError.value = true;
+      dataProcessed.value = false;
+      errorMessage.value = "Invalid URL!";
+    }
+  }
+}
+
+function checkHttps(fccUrl){
+  if (fccUrl.protocol === "https:"){
+    return true;
+  }
+}
+
+function resetValues(){
+  isError.value = false;
+  dataProcessed.value = false;
+  usesHttps.value = false;
+}
 </script>
 
 <template>
-  
+  <div class="container">
+    <h1>URL Privacy Score Generator</h1>
+    <div class="input-div">
+      <input v-model="url" placeholder="Enter your URL here" @keyup.enter="processURL">
+      <button @click="processURL">
+        <searchSVG />
+      </button>
+    </div>
+    <div class="error-message" v-if="isError">{{ errorMessage }}</div>
+    <div v-if="dataProcessed">
+      Uses Https: {{ usesHttps }} <br>
+    </div>
+  </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
+.input-div {
+  display: flex;
+  align-items: center;
+}
+.error-message {
+  color: red;
+}
+</style>
