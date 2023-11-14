7f5190b1d4c8cfff88d9eb74b075f909e2ca1dc8
anw5ph <anw5ph@virginia.edu>
2023-11-14 17:03:31 -0500

    Fixes for mac and began third party integration tracking

--- src/backend/script.py
+++ src/backend/script.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 import pandas as pd
 from flask import Flask, jsonify, request
@@ -28,8 +29,35 @@ def get_cookies():
     ChromeDriverManager().install()), options=options)
 
     driver.get(url)
-    cookies = driver.get_cookies()
 
+    # integration_elements = driver.find_elements_by_xpath('//script | //link | //iframe')
+    # for element in integration_elements:
+    #     try:
+    #         tag_name = element.tag_name
+    #         outer_html = element.get_attribute("outerHTML")
+
+    #         if tag_name == "script":
+    #             src = element.get_attribute("src")
+    #             if src and "target" not in src and (("doubleclick" in src) or ("doubleverify" in src) or ("googlesyndication" in src) or ("adservice" in src) or ("adnxs" in src) or ("adroll" in src) or ("adzerk" in src) or ("adform" in src) or ("advertising" in src) or ("adtech" in src) or ("advertising" in src)):
+    #                 print("External Script Source:", src)
+    #                 print("Element Attributes: ", outer_html)
+
+    #         elif tag_name == "link":
+    #             href = element.get_attribute("href")
+    #             if href and "target" not in href and (("doubleclick" in href) or ("doubleverify" in href) or ("googlesyndication" in href) or ("adservice" in href) or ("adnxs" in href) or ("adroll" in href) or ("adzerk" in href) or ("adform" in href) or ("advertising" in href) or ("adtech" in href) or ("advertising" in href)):
+    #                 print("External Link Href:", href)
+    #                 print("Element Attributes: ", outer_html)
+
+    #         elif tag_name == "iframe":
+    #             src = element.get_attribute("src")
+    #             if src and "target" not in src and (("doubleclick" in src) or ("doubleverify" in src) or ("googlesyndication" in src) or ("adservice" in src) or ("adnxs" in src) or ("adroll" in src) or ("adzerk" in src) or ("adform" in src) or ("advertising" in src) or ("adtech" in src) or ("advertising" in src)):
+    #                 print("External Iframe Source:", src)
+    #                 print("Element Attributes: ", outer_html)
+    #     except StaleElementReferenceException:
+    #         print("StaleElementReferenceException - Element no longer exists in the DOM")
+
+    cookies = driver.get_cookies()
+    driver.quit()
     i = 0
     cookie_json = {}
     for cookie in cookies:
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@ function processURL() {
 
       dataProcessed.value = true;
 
-      fetch('http://localhost:5000/api', {
+      fetch('http://127.0.0.1:5000/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
