344ca34a1c922086fe4f0546dce0a94864444ea0
Daniel Kweon <danielkweon19@gmail.com>
2023-11-27 22:39:04 -0500

    added scores

--- src/backend/sitesList.js
+++
@@ -1,7 +0,0 @@
-// 80 remaining sites from top 100 sites in the world
-const top100Websites = [
-    'indiatimes.com', 'dpboss.services', 'amazon.de', 
-    'mail.yahoo.com', 'webmd.com', 'xoso.com.vn', 'ikea.com', 'pt.wikipedia.org', 'sarkariresult.com', 'healthline.com', 
-    'amazon.co.jp', 'canva.com', 'amazon.co.uk', 'britannica.com', 'hurriyet.com.tr', 'doeda.com', 'ebay.com', 'speedtest.net', 
-    'adobe.com', 'ilovepdf.com'
-];
